e0b0369c3d070dc92ffb2d7596ba32c2c33a15ca
Amin Al Ali Al Darwish <amin@bunnyshell.com>
2023-11-10 15:16:09 +0400

    fixed variables

--- variables.tf
+++ variables.tf
@@ -18,7 +18,7 @@ variable "admin_user" {
 
 variable "public_key" {
   description = "Public key. This value is generated during deploy runtime and outputed into bunnyshell output"
-  default = "INPUT-PUBLIC-KEY-HERE"
+  default = "ssh-rsa AAAAB3NzaC1yc2EAAAADAQABAAACAQC6rMJu5mDnBeOjkKYwlqaHotqiC+4WiGF8RjdSicuVUlQ411rxbsEJPTcZ+FVQt0TKTMWlytUAIdzcpyo7lM+Q1y6ch/JxZcdYpIqSthfA8KaVPNbLXDKOXIbXzA864f9rb6c/bTeXUS3f3Y6thgSnABT2OTy9MXpt5iAZbi8qqmCizvpSA5hJidwENy3DQNe+MJMudZtYDNvBVovWdkSe8OKWtj+fBms/BRCPQuw0LdVvJNEYsdqwDeN78fKUMn/i44h4f77bhqnCt+w6+leRd/Z7aO1LSWikOFzkhDb5q1nENEEx5iEqrkdTL5ZkJ+h8JS3PEBlN4RhRDes6A0+ciDVUwTv8h2PTslAXALLULs3plS6KDN90EMA3PhkL2YADx57y2y9rvTBJTbBpJcXGO7LK8NLtcR+CCjgzbADLym4azbsWyCg7eTOL7TN3WnPIeYkSimI+HefrRDU7Fqj5gwyHbYtrZrnTCtykp+lm+KSP7TqfdciGE7mil6YpF/bFtQhPAufcT25xI2WM+ES+m8N/ScbxbMg37CxKj/JthxVA+6kDKjXE05CAB9T1oCkcgztbizt0FKnvdAAV5KAX7yCry/CI9esR9QQ8EzO5woATtFSE7OuJlx9gELDyukySmK7sUGQzTQBozgRhc4WvfcSulITa2x+oh8zUFQE8gQ== hello@machine.local"
 }
 
 variable "private_key_file_name" {
